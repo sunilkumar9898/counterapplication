63a7b1ec46724e4b164d1a6a235fb0793d56ccae
sunilkumar9898 <123928019+sunilkumar9898@users.noreply.github.com>
2024-04-09 12:20:43 +0530

    Update Header.jsx

--- counterapp/src/component/Header.jsx
+++ counterapp/src/component/Header.jsx
@@ -8,6 +8,8 @@ const Header = () => {
                 <Link to={"/"}>Home</Link>
                 <Link to={"/about"}>About</Link>
                 <Link to={"/contact"}>Contact</Link>
+               <Link to={"/service"}>Services</Link>
+
             </ul>
         </div>
     );
